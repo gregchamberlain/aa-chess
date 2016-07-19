8210287fe2bee44b8aa2167f4b810effff261e73
Greg Chamberlain <gregchamberlain94@gmail.com>
2016-07-18 17:47:13 -0700

    ensure steppable pieces stay on board

--- pieces/steppable.rb
+++ pieces/steppable.rb
@@ -5,8 +5,9 @@ class Steppable < Piece
   def moves(pos)
     self.class.moves_diffs.map do |move|
       [pos.first + move.first, pos.last + move.last]
-    end
+    end.select {|pos| pos.first.between?(0,7) && pos.last.between?(0,7)}
   end
+
 
 
 end
